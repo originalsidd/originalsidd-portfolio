c14fab258d78db851ca501d68445d89b4f29550b
originalsidd <originalsidd@gmail.com>
2023-04-26 20:07:41 +0530

    Made with vim

---
+++ pages/index.js
@@ -0,0 +1,21 @@
+import { Container, Box, Heading } from '@chakra-ui/react'
+
+const Page = () => {
+    return (
+        <Container>
+            <Box borderRadius="lg" bg="red" p={3} mb={6} align="center">
+                Hello, I&apos;m a full-stack developer based in India!
+            </Box>
+
+            <Box display={{ md: 'flex' }}>
+                <Box flexGrow={1}>
+                    <Heading as="h2" variant="page-title">
+                        Siddharth Pal
+                    </Heading>
+                </Box>
+            </Box>
+        </Container>
+    )
+}
+
+export default Page
